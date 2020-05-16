ac97980eee365c8013d558fd3f6b366c8020bd36
liwuyi <2020783236@qq.com>
2020-05-17 00:03:05 +0800

    发送消息接口 和拉取消息接口

--- controller/ChatController.go
+++ controller/ChatController.go
@@ -1,57 +1,35 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
+	"wechat/redis"
 	"wechat/servers/websocket"
 )
 
-type MessageDstType uint8
-type MessageType uint8
-
-const (
-	MessageDstTypeUser MessageDstType = iota
-	MessageDstTypeGroup
-)
-
-const (
-	MessageTypeUnknown MessageType = iota
-	MessageTypeText          // 文字
-	MessageTypeImage         // 图片
-	MessageTypeExpression    // 表情
-	MessageTypeVoice         // 语音
-	MessageTypeVideo         // 视频
-	MessageTypeURL           // 链接
-	MessageTypePosition      // 位置
-	MessageTypeBusinessCard  // 名片
-	MessageTypeSystem        // 系统
-	MessageTypeOther
-)
-
-type Message struct {
-	Id      	string  			`json:"id" form:"id"`           //消息ID
-	UserId  	string  			`json:"userid" form:"userId"`   //谁发的
-	DstType 	uint8  				`json:"DstType" form:"DstType"` //群聊还是私聊
-	DstID  		string  			`json:"dstid" form:"dstId"`     //对端用户ID/群ID
-	MsgType 	uint8  			`json:"msgType" form:"msgType"` //消息类型
-	Content		string 			`json:"content"` 				// 消息内容
-	Date 		int64 			`json:"date,omitempty" form:"dstId"`     	//时间
-}
 
 
 func SendMessage(c *gin.Context) {
-	jsonData := Message{}
-	err := c.BindJSON(&jsonData)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(jsonData)
-	}
+	userId := c.Query("userId")
+	data, _ := ioutil.ReadAll(c.Request.Body)
+
+	client :=  websocket.ClientManagerins.GetUserClient(userId)
+	client.SendMsg(string(data), userId)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "",
+		"errCode": 0,
+	})
 }
 
 func PullMessage(c *gin.Context) {
-
+	userId := c.Query("userId")
+	data, _ := redis.NewCache.Lrange("messageUserId_" + userId, 0 , -1)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "",
+		"errCode": 0,
+		"data": data,
+	})
 }
 
 func Chat(c *gin.Context) {
